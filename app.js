5dce0e6569211b362a58e9863a34de7746fdc960
Hong Rui <hongruiseth.teo@gmail.com>
2023-06-01 12:59:56 +0800

    Added server side validation

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-
+const Joi = require('joi');
+const {locationSchema, } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Foodloc = require('./models/foodloc');
-
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/food-where')
@@ -29,39 +29,58 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 
+const validateLocation = (req, res, next) => {
+    const { error } = locationSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
 app.get('/', (req, res) => {
     res.render('home')
 })   
+
 
 app.get('/locations', catchAsync(async (req, res) => {
     const foodlocs  = await Foodloc.find({});
     res.render('locations/index', {foodlocs})
 }))
 
+
 app.get('/locations/new', (req, res) => {
     res.render('locations/new')
 })  
 
-app.post('/locations', catchAsync(async(req, res, next) => {
+
+app.post('/locations', validateLocation, catchAsync(async(req, res, next) => {
+
     const location = new Foodloc(req.body.foodlocation)
     await location.save();
     res.redirect(`/locations/${location._id}`)
 }))
+
 
 app.get('/locations/:id', catchAsync(async (req, res) => {
     const location = await Foodloc.findById(req.params.id);
     res.render('locations/show', {location});
 }))
 
+
 app.get('/locations/:id/edit', catchAsync(async (req, res) => {
     const location = await Foodloc.findById(req.params.id);
     res.render('locations/edit', {location});
 }))
 
-app.put('/locations/:id', catchAsync(async(req, res) => {
+
+app.put('/locations/:id', validateLocation, catchAsync(async(req, res) => {
     const location = await Foodloc.findByIdAndUpdate(req.params.id, {...req.body.foodlocation});
     res.redirect(`/locations/${location._id}`);
 }))
+
 
 app.delete('/locations/:id', catchAsync(async(req, res) => {
     const {id} = req.params;
@@ -69,15 +88,18 @@ app.delete('/locations/:id', catchAsync(async(req, res) => {
     res.redirect('/locations')
 }))
 
+
 app.all('*', (req, res, next) => {
     next(new ExpressError("Page not found", 404))
 })
+
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = "Something went wrong"
     res.status(statusCode).render('error', { err })
 })
+
 
 app.listen(3000, () => {
     console.log('On port 3000')
---
+++ schemas.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+
+
+module.exports.locationSchema = Joi.object({
+    foodlocation: Joi.object({
+        title: Joi.string().required(),
+        location: Joi.string().required(),
+        image: Joi.string().required(),
+        description: Joi.string().required(),
+    }).required()
+});
