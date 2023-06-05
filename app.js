69a8333dea776097c9e2b2af0a8150342574f59f
Hong Rui <hongruiseth.teo@gmail.com>
2023-06-05 17:23:38 +0800

    Created review route

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-const {locationSchema, reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utils/catchAsync');
+// const Joi = require('joi');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Foodloc = require('./models/foodloc');
-const Review = require('./models/reviews');
 
 
 const locations = require('./routes/locations');
+const reviews = require('/locations/:id/reviews');
 
 mongoose.connect('mongodb://127.0.0.1:27017/food-where')
 
@@ -28,43 +25,14 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
 app.use("/locations", locations)
+app.use('/locations/:id/reviews', reviews)
 
-app.get('/', (req, res) => {
-    res.render('home')
-})   
-
-
-app.post('/locations/:id/reviews', catchAsync(async(req, res) => {
-   const location =  await Foodloc.findById(req.params.id);
-   const review = new Review(req.body.review);
-   location.reviews.push(review);
-   await review.save();
-   await location.save();
-   res.redirect(`/locations/${location._id}`)
-}))
-
-
-app.delete('/locations/:id/reviews/:reviewId', catchAsync(async(req, res) => {
-    const { id, reviewId } = req.params;
-    await Foodloc.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/locations/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError("Page not found", 404))
---
+++ routes/reviews.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const catchAsync = require('../utils/catchAsync');
+const { reviewSchema } = require('../schemas.js');
+const ExpressError = require('../utils/ExpressError');
+
+const Foodloc = require('../models/foodloc');
+const Review = require('../models/reviews');
+
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
+router.post('/', catchAsync(async(req, res) => {
+    const location =  await Foodloc.findById(req.params.id);
+    const review = new Review(req.body.review);
+    location.reviews.push(review);
+    await review.save();
+    await location.save();
+    res.redirect(`/locations/${location._id}`)
+ }))
+ 
+ 
+router.delete('/:reviewId', catchAsync(async(req, res) => {
+     const { id, reviewId } = req.params;
+     await Foodloc.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+     await Review.findByIdAndDelete(reviewId);
+     res.redirect(`/locations/${id}`);
+}))
+
+module.exports = router;
