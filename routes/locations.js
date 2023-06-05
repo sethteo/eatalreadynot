c0f2286ace156576cc4a087d4e28dd6efe5b2033
Hong Rui <hongruiseth.teo@gmail.com>
2023-06-05 17:15:10 +0800

    Created location route

---
+++ routes/locations.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const {locationSchema} = require('../schemas.js');
+const ExpressError = require('../utils/ExpressError');
+const Foodloc = require('../models/foodloc');
+
+
+
+const validateLocation = (req, res, next) => {
+    const { error } = locationSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
+router.get('/', catchAsync(async (req, res) => {
+    const foodlocs  = await Foodloc.find({});
+    res.render('locations/index', {foodlocs})
+}))
+
+
+router.get('/new', (req, res) => {
+    res.render('locations/new')
+})  
+
+
+router.post('/', validateLocation, catchAsync(async(req, res, next) => {
+
+    const location = new Foodloc(req.body.foodlocation)
+    await location.save();
+    res.redirect(`/locations/${location._id}`)
+}))
+
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const location = await Foodloc.findById(req.params.id).populate('reviews');
+    res.render('locations/show', {location});
+}))
+
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const location = await Foodloc.findById(req.params.id);
+    res.render('locations/edit', {location});
+}))
+
+
+router.put('/:id', validateLocation, catchAsync(async(req, res) => {
+    const location = await Foodloc.findByIdAndUpdate(req.params.id, {...req.body.foodlocation});
+    res.redirect(`/locations/${location._id}`);
+}))
+
+
+router.delete('/:id', catchAsync(async(req, res) => {
+    const {id} = req.params;
+    await Foodloc.findByIdAndDelete(id);
+    res.redirect('/locations')
+}))
+
+
+module.exports = router;
