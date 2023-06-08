dbba490fa5fbbac180ec6771ac7174760e5e7d7b
Hong Rui <hongruiseth.teo@gmail.com>
2023-06-08 11:22:13 +0800

    Added stars UX

--- routes/locations.js
+++ routes/locations.js
@@ -5,26 +5,21 @@ const { isLoggedIn, validateLocation, isAuthor } = require('../middleware');
 const catchAsync = require('../utils/catchAsync');
 const locations = require('../controllers/locations')
 
-
-router.get('/', catchAsync(locations.index));
+router.route('/')
+    .get(catchAsync(locations.index))
+    .post(isLoggedIn, validateLocation, catchAsync(locations.createLocation));
 
 
 router.get('/new', isLoggedIn, locations.renderNewForm);
 
 
-router.post('/', isLoggedIn, validateLocation, catchAsync(locations.createLocation));
-
-
-router.get('/:id', catchAsync(locations.showLocation));
+router.route('/:id')
+    .get(catchAsync(locations.showLocation))
+    .delete(isLoggedIn, isAuthor, catchAsync(locations.deleteLocation))
+    .put(isLoggedIn, isAuthor, validateLocation, catchAsync(locations.updateLocation));
 
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(locations.renderEditForm));
-
-
-router.put('/:id', isLoggedIn, isAuthor, validateLocation, catchAsync(locations.updateLocation));
-
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(locations.deleteLocation));
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,20 @@ const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users')
 
-router.get('/register', users.renderRegister);
-
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
 
-router.post('/login', storeReturnTo, 
-    passport.authenticate('local', 
-    { failureFlash: true, failureRedirect: '/login'}), 
-    users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, 
+        passport.authenticate('local', 
+        { failureFlash: true, failureRedirect: '/login'}), 
+        users.login);
 
 
 router.get('/logout', users.logout);
+
 
 module.exports = router;
