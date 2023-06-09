acf57104a1936b1f3a352d7bd8bf3c68923d2615
Hong Rui <hongruiseth.teo@gmail.com>
2023-06-09 16:25:41 +0800

    Added login and register styling

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,11 +1,13 @@
 mapboxgl.accessToken = maptoken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [103.8352, 1.3526],
     zoom: 11
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
